eed9be02e4951fec55b9df0f506e682c504932f9
CrimsonCoder42 <nostro37@gmail.com>
2024-01-11 11:19:30 -0500

    finalized basic run through

--- vite-hooks-ts-proj/src/App.tsx
+++ vite-hooks-ts-proj/src/App.tsx
@@ -1,33 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect, useCallback, MouseEvent, useMemo, useRef } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+interface User {
+  id: number;
+  username: string;
+}
+
+type fibFunc = (n: number) => number
+
+const fib: fibFunc = (n) => {
+  if (n <= 1) return 1
+  return fib(n - 1) + fib(n - 2)
+}
+
+const myNum: number = 37
+
+const App = () => {
+  const [count, setCount] = useState<number>(0)
+  const [users, setUsers] = useState<User[] | null>(null)
+
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  console.log(inputRef?.current)
+  console.log(inputRef?.current?.value)
+
+  useEffect(() => {
+    console.log('mounting')
+    console.log('Users: ', users)
+
+    return () => console.log('unmounting')
+
+  }, [])
+
+  const addTwo = useCallback((e: MouseEvent<HTMLButtonElement> | KeyboardEvent<HTMLButtonElement>):void => setCount(prev => prev + 2), [])
+
+  const result = useMemo<number>(() => fib(myNum), [myNum])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1>{count}</h1>
+    <button onClick={addTwo}>Add</button>
+    <h1>{result}</h1>
+    <input ref={inputRef} type="text" />
     </>
   )
 }
